d426cdc2e7cb07117cfe182007c221ac042c0479
Hongyang Yang <15652575067@163.com>
2021-03-29 19:54:14 +0800

    cookie session介绍和简要分析

--- community/community/community/src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ community/community/community/src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -1,14 +1,17 @@
 package com.nowcoder.community.controller;
 
 import com.nowcoder.community.service.AlphaService;
+import com.nowcoder.community.util.CommunityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
@@ -130,4 +133,44 @@ public class AlphaController {
         list.add(emp);
         return list;
     }
+
+    // cookie 示例
+    @RequestMapping(path = "/cookie/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setCookie(HttpServletResponse response) {
+        // 创建 Cookie
+        Cookie cookie = new Cookie("code", CommunityUtil.generateUUID());
+        // 设置 cookie 生效的范围
+        cookie.setPath("/community/alpha"); // 只有在/community/alpha下有效
+        // cookie 默认存在浏览器里，浏览器一关闭， cookie 就消失
+        // 但是如果经过设置的话，cookie 就会存在硬盘里，长期有效直到过期
+        cookie.setMaxAge(60 * 10); // 单位是秒
+        // 把 cookie 放到 response 中（发送cookie）
+        response.addCookie(cookie);
+
+        return "set cookie";
+    }
+    @RequestMapping(path = "/cookie/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getCookie(@CookieValue("code") String code) {
+        System.out.println(code);
+        return "get cookie";
+    }
+
+    // session 示例
+    @RequestMapping(path="/session/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setSession(HttpSession session) {
+        session.setAttribute("id", 1);
+        session.setAttribute("name", "Test");
+        return "set session";
+    }
+    @RequestMapping(path="/session/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getSession(HttpSession session) {
+        System.out.println(session.getAttribute("id"));
+        System.out.println(session.getAttribute("name"));
+        return "get session";
+    }
+
 }
