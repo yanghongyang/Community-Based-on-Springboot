ea88f25ed340a6c0af61b2a71218e5cf9c8cd349
Hongyang Yang <15652575067@163.com>
2021-04-13 10:59:00 +0800

    更新进度-设置个人主页

--- community/community/community/src/main/java/com/nowcoder/community/controller/HomeController.java
+++ community/community/community/src/main/java/com/nowcoder/community/controller/HomeController.java
@@ -4,6 +4,7 @@ import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.entity.Page;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.DiscussPostService;
+import com.nowcoder.community.service.LikeService;
 import com.nowcoder.community.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.*;
 
+import static com.nowcoder.community.util.CommunityConstant.ENTITY_TYPE_POST;
+
 @Controller
 public class HomeController {
 
@@ -21,6 +24,9 @@ public class HomeController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private LikeService likeService;
 
     @RequestMapping(path = "/index", method = RequestMethod.GET)
     public String getIndexPage(Model model, Page page) {
@@ -40,11 +46,19 @@ public class HomeController {
                 User user = userService.findUserById(post.getUserId());
                 map.put("user", user);
                 //System.out.println(user.getName());
+                long likeCount = likeService.findEntityLikeCount(ENTITY_TYPE_POST, post.getId());
+                map.put("likeCount", likeCount);
+
                 discussPosts.add(map);
             }
         }
         // addAttribute 意味着在网页上加了一个名为 discussPosts 的对象
         model.addAttribute("discussPosts", discussPosts);
         return "/index";
+    }
+
+    @RequestMapping(path = "/error", method = RequestMethod.GET)
+    public String getErrorPage() {
+        return "/error/500";
     }
 }
--- community/community/community/src/main/java/com/nowcoder/community/service/UserService.java
+++ community/community/community/src/main/java/com/nowcoder/community/service/UserService.java
@@ -1,15 +1,16 @@
 package com.nowcoder.community.service;
 
-import com.nowcoder.community.dao.LoginTicketMapper;
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.LoginTicket;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.util.CommunityConstant;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.MailClient;
+import com.nowcoder.community.util.RedisKeyUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -18,6 +19,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class UserService implements CommunityConstant {
@@ -30,8 +32,14 @@ public class UserService implements CommunityConstant {
     @Autowired
     private TemplateEngine templateEngine;
 
+/*  使用 redis 所以废弃
     @Autowired
     private LoginTicketMapper loginTicketMapper;
+    */
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     // 激活码中还需要项目名和用户名
     @Value("${community.path.domain}")
     private String domain;
@@ -39,9 +47,20 @@ public class UserService implements CommunityConstant {
     @Value("${server.servlet.context-path}")
     private String contextPath;
 
+/*
     public User findUserById(int id)
     {
         return userMapper.selectById(id);
+    }
+*/
+
+    public User findUserById(int id)
+    {
+        User user = getCache(id);
+        if(user == null) {
+           user = initCache(id);
+        }
+        return user;
     }
 
     // 注册需要返回多种情况
@@ -104,7 +123,7 @@ public class UserService implements CommunityConstant {
     }
 
     // 激活状态
-    public int activation(int userId, String code) {
+/*    public int activation(int userId, String code) {
         User user = userMapper.selectById(userId);
         if(user.getStatus() == 1) {
             // 激活之后变成1 ，现在已经是 1 说明重复
@@ -117,8 +136,24 @@ public class UserService implements CommunityConstant {
         else{
             return ACTIVATION_FAILURE;
         }
+    }*/
+    public int activation(int userId, String code) {
+        User user = userMapper.selectById(userId);
+        if(user.getStatus() == 1) {
+            // 激活之后变成1 ，现在已经是 1 说明重复
+            return ACTIVATION_REPEAT;
+        }
+        else if(user.getActivationCode().equals(code)) {
+            clearCache(userId);
+            return ACTIVATION_SUCCESS;
+        }
+        else{
+            return ACTIVATION_FAILURE;
+        }
     }
 
+    // 登录
+/*
     public Map<String, Object> login(String username, String password, int expiredSeconds) {
         Map<String, Object> map = new HashMap<>();
 
@@ -162,9 +197,139 @@ public class UserService implements CommunityConstant {
         map.put("ticket", loginTicket.getTicket());
         return map;
     }
+*/
+
+    // 重构登录功能（使用 redis）
+    public Map<String, Object> login(String username, String password, int expiredSeconds) {
+        Map<String, Object> map = new HashMap<>();
+
+        // 空值处理
+        if(StringUtils.isBlank(username)) {
+            map.put("usernameMsg", "账号不能为空！");
+            return map;
+        }
+        if(StringUtils.isBlank(password)) {
+            map.put("passwordMsg", "密码不能为空！");
+            return map;
+        }
+        // 验证账号
+        User user = userMapper.selectByName(username);
+        if(user == null) {
+            map.put("usernameMsg", "账号不存在！");
+            return map;
+        }
+        // 验证状态
+        if(user.getStatus() == 0) {
+            map.put("usernameMsg", "账号未激活！");
+            return map;
+        }
+
+        // 验证密码
+        password = CommunityUtil.md5(password + user.getSalt());
+        if(!user.getPassword().equals(password)) {
+            map.put("passwordMsg", "密码不正确！");
+            return map;
+        }
+
+        // 生成登录凭证
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(user.getId());
+        loginTicket.setTicket(CommunityUtil.generateUUID());
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + expiredSeconds * 1000));
+
+        String redisKey = RedisKeyUtil.getTicketKey(loginTicket.getTicket());
+        redisTemplate.opsForValue().set(redisKey, loginTicket);
+
+        map.put("ticket", loginTicket.getTicket());
+        return map;
+    }
 
     // 退出功能
+/*
     public void logout(String ticket) {
         loginTicketMapper.updateStatus(ticket, 1);
+    }
+*/
+
+    // 重构退出
+    public void logout(String ticket) {
+        String redisKey = RedisKeyUtil.getTicketKey(ticket);
+        LoginTicket loginTicket = (LoginTicket) redisTemplate.opsForValue().get(redisKey);
+        loginTicket.setStatus(1);
+        redisTemplate.opsForValue().set(redisKey, loginTicket);
+    }
+
+    // 查询ticket
+/*
+    public LoginTicket findLoginTicket(String ticket) {
+        return loginTicketMapper.selectByTicket(ticket);
+    }
+*/
+
+    // 重构查询ticket
+    public LoginTicket findLoginTicket(String ticket) {
+        String redisKey = RedisKeyUtil.getTicketKey(ticket);
+        return (LoginTicket) redisTemplate.opsForValue().get(redisKey);
+    }
+
+    // 更新头像
+/*    public int updateHeader(int userId, String headerUrl) {
+        return userMapper.updateHeader(userId, headerUrl);
+    }*/
+    // 重构更新头像
+    public int updateHeader(int userId, String headerUrl) {
+        int rows = userMapper.updateHeader(userId, headerUrl);
+        clearCache(userId);
+        return rows;
+    }
+
+    // 更新密码
+    public Map<String, Object> updatePassword(User user, String oldPassword, String newPassword, String confirmPassword) {
+        Map<String, Object> map = new HashMap<>();
+        // 验证密码
+        oldPassword = CommunityUtil.md5(oldPassword + user.getSalt());
+        if(!user.getPassword().equals(oldPassword)) {
+            map.put("oldPasswordMsg", "密码不正确！");
+            return map;
+        }
+        if(StringUtils.isBlank(newPassword)) {
+            map.put("newPasswordMsg", "密码不能为空！");
+            return map;
+        }
+        if(!newPassword.equals(confirmPassword)) {
+            map.put("confirmPasswordMsg", "两次输入的密码不一致！");
+            return map;
+        }
+        int id = user.getId();
+        newPassword = CommunityUtil.md5(newPassword + user.getSalt());
+        if(oldPassword.equals(newPassword)) {
+            map.put("newPasswordMsg", "旧密码和新密码一样！");
+            return map;
+        }
+        userMapper.updatePassword(id, newPassword);
+        return map;
+    }
+
+    public User findUserByName(String username) {
+        return userMapper.selectByName(username);
+    }
+
+    // 1. 优先从缓存中取值
+    private User getCache(int userId) {
+        String redisKey = RedisKeyUtil.getUserKey(userId);
+        return (User) redisTemplate.opsForValue().get(redisKey);
+    }
+    // 2. 取不到的话就初始化缓存数据
+    private User initCache(int userId) {
+        User user = userMapper.selectById(userId);
+        String redisKey = RedisKeyUtil.getUserKey(userId);
+        redisTemplate.opsForValue().set(redisKey, user, 3600, TimeUnit.SECONDS);
+        return user;
+    }
+    // 3. 数据变更的时候需要清除缓存数据
+    private void clearCache(int userId) {
+        String redisKey = RedisKeyUtil.getUserKey(userId);
+        redisTemplate.delete(redisKey);
     }
 }
