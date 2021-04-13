fa9ff22f7149e7efcbc793212907f7c7a785b6ec
Hongyang Yang <15652575067@163.com>
2021-04-13 11:00:56 +0800

    更新进度-讨论贴和帖子详情

--- community/community/community/src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
+++ community/community/community/src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -15,9 +15,10 @@ public interface DiscussPostMapper {
     // 如果方法只有一个参数，并且在<if>里使用，则必须加别名
     int selectDiscussPostRows(@Param("userId") int userId); // 查询讨论帖子行数，使用@Param 来起别名（用于动态拼接条件，并且方法只有一个条件）
 
+    int insertDiscussPost(DiscussPost discussPost);
 
+    DiscussPost selectDiscussPostById(int id);
 
-
-
+    int updateCommentCount(int id, int commentCount);
 
 }
--- community/community/community/src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ community/community/community/src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -2,8 +2,10 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -11,6 +13,9 @@ import java.util.List;
 public class DiscussPostService {
     @Autowired
     private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
 
     public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit)
     {
@@ -22,4 +27,28 @@ public class DiscussPostService {
         return discussPostMapper.selectDiscussPostRows(userId);
     }
 
+    public int addDiscussPost(DiscussPost post) {
+        if(post == null) {
+            throw new IllegalArgumentException("参数不能为空！");
+        }
+        // 过滤敏感词
+        // 除此之外还需要去掉标签，诸如<script></script>
+
+        // 转义 HTML 标记
+        post.setTitle(HtmlUtils.htmlEscape(post.getTitle()));
+        post.setContent(HtmlUtils.htmlEscape(post.getContent()));
+        // 过滤敏感词
+        post.setTitle(sensitiveFilter.filter(post.getTitle()));
+        post.setContent(sensitiveFilter.filter(post.getContent()));
+
+        return discussPostMapper.insertDiscussPost(post);
+    }
+
+    public DiscussPost findDiscussPostById(int id) {
+        return discussPostMapper.selectDiscussPostById(id);
+    }
+
+    public int updateCommentCount(int id, int commentCount) {
+        return discussPostMapper.updateCommentCount(id, commentCount);
+    }
 }
